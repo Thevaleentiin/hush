a2ae05a7c2193918311827e4f66d8bf6e6d3326e
Kahn-Barberon Valentin <kahnba_v16@myges.fr>
2018-12-30 15:08:21 +0100

    meta responsive

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+
+
+ ?>
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <title>Hush</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  </head>
+  <body>
+
+  </body>
+</html>
